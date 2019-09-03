5f58db8c814a21257d494264b53451c69e25e7e2
Danilo Rossi <contact@danilorossi.me>
2019-09-03 20:29:08 +0200

    Updates

---
+++ precache-manifest.2011d92593bf7f083c316853b422a55d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee72c602e88e8bcdd5ea2cd20fc1b6a7",
+    "url": "/creative-coding-playground/index.html"
+  },
+  {
+    "revision": "d316e6244fe990729e6d",
+    "url": "/creative-coding-playground/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "275e040a3ec98796f6e2",
+    "url": "/creative-coding-playground/static/js/2.3a143122.chunk.js"
+  },
+  {
+    "revision": "d316e6244fe990729e6d",
+    "url": "/creative-coding-playground/static/js/main.732b62d6.chunk.js"
+  },
+  {
+    "revision": "aa9ce16ed384d84ce38c",
+    "url": "/creative-coding-playground/static/js/runtime~main.f04d894c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/creative-coding-playground/precache-manifest.bf7c42aabbf7b5c8928b0869c89728f1.js"
+  "/creative-coding-playground/precache-manifest.2011d92593bf7f083c316853b422a55d.js"
 );
 
 self.addEventListener('message', (event) => {
